2e14c232e68d6652d490e5e90b007b4fac957e82
joseML2020 <josemoroluque@gmail.com>
2024-05-06 10:34:16 +0200

    Add config
    Add start, dev, and prod scripts; move source code into a src directory
    
    The package.json file has been updated with new "start", "dev", and "prod" scripts. The source code has been moved into a new "

---
+++ config.js
@@ -0,0 +1,18 @@
+
+const dotenv = require('dotenv')
+const path = require('path');
+
+dotenv.config({
+  path: path.resolve(__dirname, process.env.NODE_ENV + '.env')
+});
+
+module.exports = {
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    HOST:  process.env.HOST || '127.0.0.1',
+    PORT:  process.env.PORT || 8443,
+    MONGODB_USER: process.env.MONGODB_USER || 'root',
+    MONGODB_PASSWORD: process.env.MONGODB_PASSWORD || 'root',
+    MONGODB_HOST: process.env.MONGODB_HOST || 'localhost' ,
+    MONGODB_DATABASE: process.env.MONGODB_DATABASE || 'crud',
+    MONGODB_PORT: process.env.MONGODB_PORT || 27017
+  }
